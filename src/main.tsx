b7c3b54287ccdc59f19462d8e2b2c961d22204ca
Mario Garcia <msgimanila@yahoo.com>
2024-12-08 15:55:36 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
- 
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// src/main.tsx or src/App.tsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
